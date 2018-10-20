300698598cbb9c21e511d67e522b8dee048b7375
Marvel Wild <marvel.me@gmail.com>
2018-10-20 21:59:15 +0300

    +vegetation

--- entity/world/flower.lua
+++ entity/world/flower.lua
@@ -10,7 +10,9 @@ _.new=function(options)
 	
 	local spriteName=Lume.randomchoice({"flower_yellow", "flower_yellow_2","flower_red","blueberry","strawberry",
 			"carrot_small","wheat_1","cactus_1", "watermelon",
-			"bush_1"})	
+			"bush_1","grass_1","grass_2","grass_3",
+			"grass_4","grass_5","grass_6","grass_7","flower_camella", "mushroom_1","wheat_2"})
+	
 	Entity.setSprite(result,spriteName)
 	result.entity="Flower"
 	result.x=0
