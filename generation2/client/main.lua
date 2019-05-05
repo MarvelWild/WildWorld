c1a677384614831bb21415b86b6018c1980b0647
Marvel Wild <marvel.me@gmail.com>
2019-05-05 22:38:08 +0300

    +camera. can travel around level

--- generation2/client/main.lua
+++ generation2/client/main.lua
@@ -15,9 +15,17 @@ ConfigService=require("shared.entity.service.config")
 ClientService=require("entity.service.client_service")
 GameService=require("entity.service.game")
 GameState=require("entity.service.game_state")
+Gamera=require("shared.lib.gamera.gamera")
+Cam = Gamera.new(0,0,128,128)
+
+
+local doDraw=function()
+	Pow.draw()
+end
+
 
 love.draw=function()
-	Pow.draw()
+	Cam:draw(doDraw)
 end
 
 love.load=function()
@@ -25,19 +33,36 @@ love.load=function()
 	netState.isServer=false
 	netState.isClient=true
 	
+	love.graphics.setDefaultFilter( "nearest", "nearest" )
+	
 	Entity.add(GameService)
 	Entity.add(ClientService)
 	GameService.start()
+	Cam:setScale(4)
+	-- todo
+	Cam:setWorld(0,0,4096,4096)
 end
 
 love.update=function()
+	local player=GameState.getPlayer()
+	if player~=nil then
+		Cam:setPosition(player.x, player.y)	
+		log("cam set pos:"..player.x..','..player.y)
+		log("cam pos:"..Cam:getPosition())
+	end
 	Pow.update()
 end
 
 love.mousepressed=function(x,y,button,istouch)
-	GameService.mousepressed(x,y,button,istouch)
+	local gameX,gameY
+	gameX,gameY=Cam:toWorld(x,y)
+	
+	GameService.mousepressed(gameX,gameY,button,istouch)
 end
 
+love.resize=function(width, height)
+	Cam:setWindow(0,0,width,height)
+end
 
 
 love.quit=function()
