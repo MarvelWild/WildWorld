818489fb11c11d47f034d0c5c3d84d57b16c927b
Marvel Wild <marvel.me@gmail.com>
2020-04-16 23:23:17 +0300

    no message

--- generation2/shared/entity/trait/growable.lua
+++ generation2/shared/entity/trait/growable.lua
@@ -5,7 +5,7 @@ duck typing:
 local _={}
 
 _.do_grow=function(entity,sprite_name)
-	log("Growable.do_grow. sprite:"..sprite_name.." ent:"..Inspect(entity))
+--	log("Growable.do_grow. sprite:"..sprite_name.." ent:"..Inspect(entity))
 	entity.sprite=sprite_name
 	entity.grow_phase_index=entity.grow_phase_index+1
 end
